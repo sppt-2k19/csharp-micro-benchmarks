bd5939685930e7563753f6565894e7dd64612542
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-02-22 13:23:00 +0100

    init

---
+++ CSharp-Microbenches/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace CSharp_Microbenches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var iterations = 5;
+            var minTime = 250 * 1000000.0;
+            var result = 0.0;
+            
+            result += Mark8("ScaleVector2D", Tests.ScaleVector2D, iterations, minTime);
+            result += Mark8("ScaleVector3D", Tests.ScaleVector3D, iterations, minTime);
+            result += Mark8("MultiplyVector2D", Tests.MultiplyVector2D, iterations, minTime);
+            result += Mark8("MultiplyVector3D", Tests.MultiplyVector3D, iterations, minTime);
+            result += Mark8("TranslateVector2D", Tests.TranslateVector2D, iterations, minTime);
+            result += Mark8("TranslateVector3D", Tests.TranslateVector3D, iterations, minTime);
+            result += Mark8("SubtractVector2D", Tests.SubtractVector2D, iterations, minTime);
+            result += Mark8("SubtractVector3D", Tests.SubtractVector3D, iterations, minTime);
+            result += Mark8("LengthVector2D", Tests.LengthVector2D, iterations, minTime);
+            result += Mark8("LengthVector3D", Tests.LengthVector3D, iterations, minTime);
+            result += Mark8("DotProductVector2D", Tests.Dotproduct2D, iterations, minTime);
+            result += Mark8("DotProductVector3D", Tests.Dotproduct3D, iterations, minTime);
+            
+            Console.WriteLine("\n" + result);
+        }
+        
+        public static double Mark8(string msg, Func<int, double> fun,
+                    int iterations, double minTime)
+                {
+                    int count = 1, totalCount = 0;
+                    double dummy = 0.0, runningTime = 0.0, deltaTime = 0.0, deltaTimeSquared = 0.0;
+                    do
+                    {
+                        count *= 2;
+                        deltaTime = 0.0;
+                        deltaTimeSquared = 0.0;
+                        for (var j = 0; j < iterations; j++)
+                        {
+                            var sw = System.Diagnostics.Stopwatch.StartNew();
+                            for (var i = 0; i < count; i++)
+                            {
+                                dummy += fun(i);
+                            }
+                            runningTime = sw.ElapsedTicks * 100.0f;
+                            var time = runningTime / count;
+                            deltaTime += time;
+                            deltaTimeSquared += time * time;
+                            totalCount += count;
+                        }
+                    } while (runningTime < minTime && count < int.MaxValue / 2);
+        
+                    var mean = deltaTime / iterations;
+                    var standardDeviation = Math.Sqrt((deltaTimeSquared - mean * mean * iterations) / (iterations - 1));
+                    Console.WriteLine($"{msg};{mean};{standardDeviation};{count}"
+                        .Replace(',', '.')
+                        .Replace(';', ',')
+                    );
+        //            Console.WriteLine($"{msg} done");
+                    return dummy / totalCount;
+                }
+    }
+}
---
+++ CSharp-Microbenches/Tests.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace CSharp_Microbenches
+{
+    static class Tests
+    {
+        public static double ScaleVector2D(int scalar){
+            var v = new Vector2(1);
+            v *= scalar;
+            return v.Length();
+        }
+
+        public static double ScaleVector3D(int scalar){
+            var v = new Vector3(1);
+            v *= scalar;
+            return v.Length();
+        }
+
+        public static double MultiplyVector2D(int i){
+            var v1 = new Vector2(1);
+            var v2 = new Vector2(i);
+            v1 *= v2;
+            return v1.Length();
+        }
+
+        public static double MultiplyVector3D(int i){
+            var v1 = new Vector3(1);
+            var v2 = new Vector3(i);
+            v1 *= v2;
+            return v1.Length();
+        }
+
+        public static double TranslateVector2D(int i)
+        {
+            var v1 = new Vector2(1);
+            var v2 = new Vector2(i);
+            var v3 = v1 + v2;
+            return v3.Length();
+        }
+
+        public static double TranslateVector3D(int i){
+            var v1 = new Vector3(1);
+            var v2 = new Vector3(i);
+            var v3 = v1 + v2;
+            return v3.Length();
+        }
+
+        public static double SubtractVector2D(int i){
+            var v1 = new Vector2(i);
+            var v2 = new Vector2(1);
+            v1 -= v2;
+            return v1.Length();
+        }
+
+        public static double SubtractVector3D(int i){
+            var v1 = new Vector2(i);
+            var v2 = new Vector2(1);
+            v1 -= v2;
+            return v1.Length();
+        }
+
+        public static double LengthVector2D(int i){
+            var v1 = new Vector2(i);
+            return v1.Length();
+        }
+
+        public static double LengthVector3D(int i){
+            var v1 = new Vector3(i);
+            return v1.Length();
+        }
+
+        public static double Dotproduct2D(int i){
+            var v1 = new Vector2(1);
+            var v2 = new Vector2(i);
+            return Vector2.Dot(v1, v2);
+        }
+
+        public static double Dotproduct3D(int i){
+            var v1 = new Vector3(1);
+            var v2 = new Vector3(i);
+            return Vector3.Dot(v1, v2);
+        }
+        
+        public static double Primes(int number)
+        {
+            var realNumber = 100;
+            
+            var A = new bool[realNumber + 1];
+            for (int i = 2; i < realNumber + 1; i++)
+            {
+                A[i] = true;
+            }
+
+            for (int i = 2; i < Math.Sqrt(realNumber); i++)
+            {
+                if(A[i])
+                {
+                    var iPow = (int) Math.Pow(i, 2);
+                    var num = 0;
+
+                    for (int j = 0; j < realNumber; j = iPow + num * i)
+                    {
+                        A[i] = false;
+                        num++;
+                    }
+                }
+            }
+
+            var primes = new List<int>();
+            for (int i = 2; i < A.Length; i++)
+            {
+                if (A[i])
+                    primes.Add(i);
+            }
+
+            return primes.Count & number;
+        }
+
+        public static double MemTest(int i)
+        {
+            var array = new double[100000];
+            return array.Length + i;
+        }
+        
+        public static double Sestoft(int input)
+        {
+            var x = 1.1 * (input & 0xFF);
+            return x * x * x * x * x * x * x * x * x * x * x * x * x * x * x * x * x * x * x * x;
+        }
+    }
+}
